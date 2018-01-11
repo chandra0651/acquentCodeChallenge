db36755851bfbf52544e59a542425317b04c57d3
Chandra Reddy Kosna <ckosna@Egens-MacBook-Pro-2.local>
2018-01-11 15:49:36 -0600

    Fixed Tests

--- src/test/java/com/acquent/test/acquenttest/PizzaUnitTest.java
+++ src/test/java/com/acquent/test/acquenttest/PizzaUnitTest.java
@@ -5,18 +5,20 @@ import com.acquent.test.acquenttest.Order;
 import com.acquent.test.acquenttest.Pizza;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+@RunWith(SpringRunner.class)
 public class PizzaUnitTest {
 
-    @Autowired
-    Pizza pizza;
+    Pizza pizza = new Pizza();
 
     @Test
     public void testCurrentDate() {
